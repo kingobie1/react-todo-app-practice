e3c05510c82c396d40097fb18e90ddb7a7f13c45
Andrew Rottier <andrewrottier95@gmail.com>
2017-12-12 15:40:30 -0500

    Code commented and clearned

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,6 +4,23 @@ import TextForm from './TextForm'
 import Item from './Item'
 
 export default class Todo extends React.Component {
+
+    /* This is our constructor for Todo component
+     *
+     * props - as of right now this class is not passed any props, however, we set up our code
+     * to allow future propegation of props from the root component.
+     *
+     * state - state variables are local to this instance/mount of this component. Whenever a state variable
+     * is changed, the component will automatically re-render itself to include state variables changes.
+     *
+     * function binding - technically, none of our functions are referencing the component's state variables,
+     * so there is no need to bind the functions. Below is an example of binding a function:
+     *
+     *          this.generateRenderItemListItem = this.generateRenderItemListItem.bind(this);
+     *
+     * This allows the function to reference functions and variables that are also bounded to the component.
+     *
+     */
     constructor(props) {
         super(props);
 
@@ -18,6 +35,12 @@ export default class Todo extends React.Component {
         };
     }
 
+    /**
+     * generateRenderItemListItem - filter through the items and generate the react components for all in incompleted items
+     * @param items - all items from storage
+     * @param fx - function relative to this item
+     * @returns {Array|Object|*}
+     */
     generateRenderItemListItem(items, fx) {
         return items.filter(i => !i.isComplete).map(function(item){
             return  (
@@ -30,6 +53,12 @@ export default class Todo extends React.Component {
         });
     }
 
+    /**
+     * generateRenderDoneItemListItem - filter through the items and generate the react components for all in completed items
+     * @param items - all items from storage
+     * @param fx - function relative to this item
+     * @returns {Array|Object|*}
+     */
     generateRenderDoneItemListItem(items, fx) {
         return items.filter(i => i.isComplete).map(function(item){
             return  (
@@ -42,10 +71,19 @@ export default class Todo extends React.Component {
         });
     }
 
+    /**
+     * handleItemClick - toggle the state of the item
+     * @param itemData
+     */
     handleItemClick(itemData) {
         this.toggleCompleted(itemData);
     }
 
+    /**
+     * toggleCompleted - find the index of the item to toggle and then toggle it - since items are not always linked to a
+     * specific index in items
+     * @param itemToToggle
+     */
     toggleCompleted(itemToToggle) {
         let i = this.state.items.indexOf(itemToToggle);
         let items = this.state.items.slice();
@@ -54,6 +92,10 @@ export default class Todo extends React.Component {
         this.setState({items: items});
     }
 
+    /**
+     * addItem take in an item
+     * @param itemDescription description from the input box
+     */
     addItem(itemDescription) {
         let items = this.state.items.slice();
         const newItem = {desc: itemDescription, isComplete: false};
@@ -61,25 +103,36 @@ export default class Todo extends React.Component {
         this.setState({items: items});
     }
 
+    /**
+     * clearCompleted - filter through the items and remove all that are not completed yet
+     */
     clearCompleted() {
         let items = this.state.items.slice();
         this.setState({items: items.filter(i => !i.isComplete)});
     }
 
-    handleItemSubmittedFromForm(itemDescription) {
-        this.addItem(itemDescription);
-    }
 
+    /**
+     * componentDidUpdate is a react built in method that is invoked whenever something on the DOM has changed.
+     * It is not called on the initial render
+     */
     componentDidUpdate() {
         this.saveCookie();
     }
 
+    /**
+     * saveCookie store the current state of the items in our cookie
+     */
     saveCookie() {
         const cookies = new Cookies();
         cookies.set('items', this.state.items, { path: '/' });
         console.log(cookies.get('items'));
     }
 
+    /**
+     * render is invoked whenever a state variable is updated and allows the page to refresh with any changes.
+     * @returns {XML}
+     */
     render() {
         var itemsList = this.generateRenderItemListItem(this.state.items, (item) => this.handleItemClick(item));
         var doneItemsList = this.generateRenderDoneItemListItem(this.state.items, (item) => this.handleItemClick(item));
@@ -88,7 +141,7 @@ export default class Todo extends React.Component {
             <div id="container">
                 <h1>To-Do List</h1>
                 <TextForm
-                    submitItem={(itemDescription) => this.handleItemSubmittedFromForm(itemDescription)}
+                    submitItem={(itemDescription) => this.addItem(itemDescription)}
                 />
 
                 <ul>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import React from 'react'
 import Todo from './Todo'
 import './index.css';
 
-/*
-    resources:
-        REACT tutorial - https://reactjs.org/tutorial/tutorial.html
-        REACT input - https://reactjs.org/docs/forms.html
- */
-
-// ========================================
 ReactDOM.render(
     <Todo />,
     document.getElementById('root')
